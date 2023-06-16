64d9608525d832cf38492507e28e6010a56ae826
Tayyab-Kamboh <tayyabkambohSK28@gmail.com>
2023-06-16 05:26:14 +0200

    Add files

---
+++ morse_code.rb
@@ -0,0 +1,42 @@
+MORSE_CODE = {
+  'A' => '.-',
+  'B' => '-...',
+  'C' => '-.-.',
+  'D' => '-..',
+  'E' => '.',
+  'F' => '..-.',
+  'G' => '--.',
+  'H' => '....',
+  'I' => '..',
+  'J' => '.---',
+  'K' => '-.-',
+  'L' => '.-..',
+  'M' => '--',
+  'N' => '-.',
+  'O' => '---',
+  'P' => '.--.',
+  'Q' => '--.-',
+  'R' => '.-.',
+  'S' => '...',
+  'T' => '-',
+  'U' => '..-',
+  'V' => '...-',
+  'W' => '.--',
+  'X' => '-..-',
+  'Y' => '-.--',
+  'Z' => '--..',
+}.freeze
+
+def decode_char(char)
+  MORSE_CODE.key(char)
+end
+
+def decode_word(word)
+  word.split.map { |char| decode_char(char) }.join
+end
+
+def decode_morse(message)
+  message.split('   ').map { |word| decode_word(word) }.join(' ')
+end
+
+puts decode_morse('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
